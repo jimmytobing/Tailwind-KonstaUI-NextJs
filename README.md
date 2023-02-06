cff0b26d53c87af9b83d1f5a9af8d578f9838703
jimmytobing <jimmy.tobing@gmail.com>
2023-02-06 22:58:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind-KonstaUI-NextJs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-bxfm2w)
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// import konstaConfig config
+const konstaConfig = require('konsta/config');
+
+// wrap config with konstaConfig config
+module.exports = konstaConfig({
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media', // or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+});
